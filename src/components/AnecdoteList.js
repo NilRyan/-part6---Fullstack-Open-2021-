76561ecb659a137bcdb0975f2ecb806b751f3d16
Neil Ryan <neilryanlipaod@gmail.com>
2021-03-20 13:35:08 +0800

    6.8: anecdotes, step6
    REFACTORED logic for rendering anecdote list to its own component
    AnecdoteList.

---
+++ src/components/AnecdoteList.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { vote } from '../reducers/anecdoteReducer'
+
+const Anecdote = ({anecdote, handleVote}) => {
+  return(
+    <li>
+      <div>
+          {anecdote.content}
+        </div>
+        <div>
+          has {anecdote.votes}
+          <button onClick={() => handleVote(anecdote.id)}>vote</button>
+      </div>
+    </li>
+   
+  )
+}
+const AnecdoteList = () => {
+  const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  const handleVote = (id) => {
+    console.log('vote', id)
+    dispatch(vote(id))
+  }
+  return (
+    <div>
+      { anecdotes
+        .sort((a, b) => b.votes - a.votes)
+        .map(anecdote =>
+        <Anecdote key={anecdote.id} anecdote={anecdote} handleVote ={handleVote} />
+      )}
+    </div>
+    
+  )
+}
+
+export default AnecdoteList
